69dcc2ec499e5af767f084c2dbc9fb405e4f8a91
Temporary SDA User 1164 <tmp-sda-1164@kth.se>
2018-03-19 12:18:53 +0100

    Initial commit

---
+++ Vehicle_Inspection_Exercise-Group4/src/sda3/group4/iv/startup/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Vehicle Inspection Application
+ */
+package sda3.group4.iv.startup;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+
+}
